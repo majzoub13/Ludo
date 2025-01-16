d032f661c683f16ecaa9bfa990b5b5b61568216f
ThePhenomenal-Z <alzaintab@gmail.com>
2025-01-16 14:56:40 +0300

    some fixes on player

--- Player.py
+++ Player.py
@@ -24,18 +24,19 @@ class Player:
 
     def get_home_piece(self):
         for piece in self.pieces:
-            if piece.pos is None:
+            if piece.pos is None and piece.safe is None:
                 return piece
 
     def get_movable_pieces(self):
-        movable_pieces = [piece for piece in self.pieces if piece.pos is not None]
+        movable_pieces = [piece for piece in self.pieces if piece.pos is not None or piece.safe is not None]
+        print(movable_pieces)
         return movable_pieces
 
     def is_home_empty(self):
-        return not any(piece.pos is None for piece in self.pieces)
+        return not any(piece.pos is None and piece.safe is None for piece in self.pieces)
 
     def all_pieces_in_home(self):
-        return all(piece.pos is None for piece in self.pieces)
+        return all(piece.pos is None and piece.safe is None for piece in self.pieces)
 
     def __str__(self):
         return f"Player Team: {self.team} Turn: {self.turn}"
